c177ca0385747bb6b1494dc644691645c4a1e2fb
Dan <kovarik.daniel@gmail.com>
2019-09-13 07:02:14 +0000

    fixed byobu tmux colors

--- setup.sh
+++ setup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-sudo apt install vim-gtk
+sudo apt install -y vim-gtk
+echo 'Creating vim dir'
+mkdir ${HOME}/.vim/
+echo 'Creating bundle dir'
+mkdir ${HOME}/.vim/bundle/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-sudo apt install byobu
-set -g default-terminal "xterm-256color"
 
 {
-	echo "'set -g default-terminal ${TERM}'" >> "$HOME/.byobu/.tmux.conf"
+	echo "set -g default-terminal '${TERM}'" >> "$HOME/.byobu/.tmux.conf"
 } ||
 {
-	echo "'set -g default-terminal ${TERM}'" >> "$HOME/.config/byobu/.tmux.conf"
+	echo "set -g default-terminal '${TERM}'" >> "$HOME/.config/byobu/.tmux.conf"
 
 }
+sudo apt install -y byobu
 mkdir $HOME/.vim/colors
 wget https://raw.githubusercontent.com/blueshirts/darcula/master/colors/darcula.vim -P $HOME/.vim/colors
-ln -s $HOME/server-setup-editor/.vimrc ~/.vimrc
+ln -s $HOME/server-setup-editor/.vimrc $HOME/.vimrc
 sudo cp gdrive /usr/bin/
 vim -c PluginInstall
